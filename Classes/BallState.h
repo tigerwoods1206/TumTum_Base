61cfb91b5bc94089719179a2ce80be6c421ad574
ohtaisao <ohtaisao@square-enix.com>
2014-12-15 18:58:30 +0900

    リソース追加

--- Classes/BallState.h
+++ Classes/BallState.h
@@ -13,13 +13,48 @@
 
 using namespace cocos2d;
 
-class BallState : public <#parent class#>
-{
+class StateBase :public Node{
+protected:
+    enum Trans {
+        Trans_Me_Again,   // 再び自分
+        Trans_Occured,    // 状態遷移が起きた
+    };
+    
 public:
-    virtual bool init();
-    static BallState *createBallState();
-    CREATE_FUNC(BallState);
-    <#member functions...#>
+    StateBase() {}
+};
+
+template< class VAL >
+class BallState : public StateBase {
+private:
+    bool bFirstUpdate;
+    
+protected:
+    virtual void entryAction(VAL& outerVal) {}
+    virtual Trans inputAction(VAL& outerVal, BallState<VAL>  &outNextState) {return Trans_Me_Again;}
+    virtual void exitAction(VAL& outerVal) {}
+    
+    //touch
+    void onTouchesBegan(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event *event);
+    void onTouchesMoved(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event *event);
+    void onTouchesEnded(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event *event);
+    
+public:
+    BallState() : bFirstUpdate(true) {}
+    
+    // 状態更新
+    
+    BallState<VAL>  update(VAL& outerVal) {
+        if (bFirstUpdate) {
+            entryAction(outerVal);
+            bFirstUpdate = false;
+        }
+        BallState<VAL> outerState;
+        Trans trans = inputAction(outerVal, outerState);
+        if (trans == Trans_Occured)
+            exitAction(outerVal);
+        return outerState;
+    }
 };
 
 #endif /* defined(__TumTum_Base__BallState__) */
--- Classes/BallTouchState.h
+++ Classes/BallTouchState.h
@@ -10,16 +10,45 @@
 #define __TumTum_Base__BallTouchState__
 
 #include "cocos2d.h"
+#include "BallState.h"
 
 using namespace cocos2d;
 
-class BallTouchState : public <#parent class#>
+class BallTouchState : public BallState<bool>
 {
+    
+    class BallTouch : public BallState<BallTouchState>
+    {
+    public:
+        virtual bool init();
+        static BallTouch *createBallTouchState();
+        CREATE_FUNC(BallTouch);
+        
+    };
+
+    
 public:
     virtual bool init();
     static BallTouchState *createBallTouchState();
     CREATE_FUNC(BallTouchState);
-    <#member functions...#>
+    virtual void entryAction(BallTouchState& outerVal) {
+        
+    };
+    virtual Trans inputAction(BallTouchState& outerVal, BallState<BallTouchState>  &outNextState) {
+        //outNextState =
+        return StateBase::Trans_Me_Again;
+    }
+    virtual void exitAction(BallTouchState& outerVal);
 };
+
+
+class BallAleadyTouch : public BallState<bool>
+{
+public:
+    virtual bool init();
+    static BallAleadyTouch *createBallTouchState();
+    CREATE_FUNC(BallAleadyTouch);
+};
+
 
 #endif /* defined(__TumTum_Base__BallTouchState__) */
